84b345d8e0fdda68c3c94afb4adfdad9089f7118
chetan <chetan249581@gmail.com>
2023-09-20 19:53:11 +0530

    scroll behaviour and contact is added

--- app.js
+++
@@ -1,6 +0,0 @@
-// let modal=document.getElementById('modal');
-
-function fun(){
-    // document.getElementById("modal").innerHTML = "I have changed!";
-    document.getElementsByClassName("one").style.display="none";
-}
