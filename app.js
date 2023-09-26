8b3db2c2b9d7385e9170c7e6af03c8b5c0c5027f
chetan <chetan249581@gmail.com>
2023-09-26 20:45:47 +0530

    Added Event listner to one solution

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const one=document.getElementById("one");
+
+
+// function fun(){
+//     one.innerHTML="HEllo";
+// }
+// one.addEventListener("click",fun());
+
+one.addEventListener("click", function() {
+    document.getElementById("sol1").innerHTML = "Governments and private companies can invest in expanding public charging networks, including Level 2 chargers and DC fast chargers, in urban and rural areas.Incentivize businesses to install charging stations in their parking lots to attract EV-owning customers.";
+  });
